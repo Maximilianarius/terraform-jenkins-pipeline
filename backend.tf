601a2f0d53405002ddf111c8fb80a5d177cf9733
Maximilianarius <MFlorea@lomixtech.com>
2020-11-19 18:17:08 -0500

    changed the backend file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.13.0"
   backend "s3" {
-    bucket = "go-green-insurance"
+    bucket = "terraform-hold-state"
     key    = "s3/jenkins/terraform.tfstate"
     region = "us-east-2"
   }
