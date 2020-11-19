e5bf78af1de6207b9634559bb8d223656e7d089d
Maximilianarius <MFlorea@lomixtech.com>
2020-11-19 18:21:05 -0500

    changed the region

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 0.13.0"
   backend "s3" {
-    bucket = "terraform-hold-state"
+    bucket = "cf-templates-1u9aosc0wisnc-us-east-2"
     key    = "s3/jenkins/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
