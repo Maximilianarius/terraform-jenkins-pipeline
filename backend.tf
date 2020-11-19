49f9e87530018b1ff6428bdde6e21e272b9700de
Maximilianarius <68789046+Maximilianarius@users.noreply.github.com>
2020-11-19 18:01:54 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.13.0"
   backend "s3" {
-    bucket = "terraform-up-and-running-state-superb-gelding"
+    bucket = "go-green-insurance"
     key    = "s3/jenkins/terraform.tfstate"
     region = "us-east-2"
   }
